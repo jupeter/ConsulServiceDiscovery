ff10e9626a3eec6ae42bfd6d87a46032204092ff
Piotr Plenik <piotr.plenik.consultant@nielsen.com>
2019-12-26 23:27:01 +0100

    initialize project

---
+++ ConsulBuilder.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Consul;
+using System;
+
+namespace ConsulServiceDiscovery
+{
+    public static class ConsulBuilder
+    {
+        public static IServiceCollection AddConsul(this IServiceCollection services, IConfiguration configuration)
+        {
+            if (!configuration.GetSection("consulConfig").Exists())
+                return services;
+
+            services.AddSingleton<Microsoft.Extensions.Hosting.IHostedService, ConsulHostedService>();
+            services.Configure<ConsulConfig>(configuration.GetSection("consulConfig"));
+            services.AddSingleton<IConsulClient, ConsulClient>(p => new ConsulClient(consulConfig =>
+            {
+                var address = configuration["consulConfig:address"];
+                consulConfig.Address = new Uri(address);
+            }));
+
+            return services;
+        }
+
+        public static async void UnregisterFromConsul(IConfiguration configuration)
+        {
+            if (!configuration.GetSection("consulConfig").Exists())
+                return;
+
+            ConsulClient client = new ConsulClient(consulConfig =>
+            {
+                consulConfig.Address = new Uri(configuration["consulConfig:address"]);
+            });
+
+            await client.Agent.ServiceDeregister(ConsulHostedService.RegistrationID);
+        }
+    }
+}
